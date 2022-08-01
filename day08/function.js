805545bee9e845f395e5bdaef2984530149cfe8c
lee2-hj <hj199508@gmail.com>
2022-08-01 16:21:37 +0900

    연습

---
+++ day08/function.js
@@ -0,0 +1,6 @@
+console.log(`------------------------------------`);
+console.log(`function.js 내부에 실행되는 코드`);
+console.log(`------------------------------------`);
+function outerfunction(){
+    console.log(`function.js에서 선언된 함수`);
+}
